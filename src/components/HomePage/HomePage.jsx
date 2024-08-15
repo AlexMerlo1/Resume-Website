2bfa102a5e10236246a90e8d7b7f857d61de2113
AlexMerlo1 <113566319+AlexMerlo1@users.noreply.github.com>
2024-08-15 19:36:09 -0400

    Photo change

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -21,17 +21,17 @@ const HomePage = () => {
           </div>
         </div>
         <div className="my-picture">
-          <img src="src/assets/placeholderHeadshot.png" alt="Picture of Alex Merlo" />
+          <img src="src\assets\headshot.png" alt="Picture of Alex Merlo" />
         </div>
       </div>
       <div className='technologies-container'>
         <h3>My Skills</h3>
         <div className='technologies-icons'>
-          <TechIcon src="src/assets/Technology Icons/Java-Logo.png" alt="Java Logo"/>
-          <TechIcon src="src\assets\Technology Icons\python-logo.png" alt="Python Logo" />
-          <TechIcon src="src\assets\Technology Icons\qlik-logos.png" alt="Qlik Logo" />
-          <TechIcon src="src\assets\Technology Icons\C_LOGO.png" alt="C Logo" />
-          <TechIcon src="src\assets\Technology Icons\javascriptt-logo.png" alt="JavaScript Logo" />
+          <TechIcon src="src/assets/Java-Logo.png" alt="Java Logo"/>
+          <TechIcon src="src\assets\python-logo.png" alt="Python Logo" />
+          <TechIcon src="src\assets\qlik-logos.png" alt="Qlik Logo" />
+          <TechIcon src="src\assets\C_LOGO.png" alt="C Logo" />
+          <TechIcon src="src\assets\javascriptt-logo.png" alt="JavaScript Logo" />
         </div>
       </div>
     </div>
